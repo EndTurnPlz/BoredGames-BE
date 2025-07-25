f9b46d72040ab4c45584783b1a83aead997a2636
Anish <anishisreddy95@gmail.com>
2025-07-25 18:14:54 -0400

    More unit tests

--- test/unit/BoredGames.UnitTests.Apologies/Board/GameBoardTest.cs
+++ test/unit/BoredGames.UnitTests.Apologies/Board/GameBoardTest.cs
@@ -46,18 +46,22 @@ public class GameBoardTest
     [Fact]
     public void TryExecuteSplitMove_ValidSplitMove_ShouldReturnTrue()
     {
-        
-        _gameBoard.PawnTiles[0][0] = BoardTileDfs("a_1")!;
-        _gameBoard.PawnTiles[0][1] = BoardTileDfs("a_3")!;
-        
+        // Arrange
+        _gameBoard.PawnTiles[0][0] = BoardTileDfs(_gameBoard, "a_1")!;
+        _gameBoard.PawnTiles[0][1] = BoardTileDfs(_gameBoard, "a_3")!;
+
         var split2Effect = GetMoveEffect("Split2");
         var split5Effect = GetMoveEffect("Split5");
         var firstMove = new GenericComponents.Move("a_1", "a_s1", split2Effect);
         var secondMove = new GenericComponents.Move("a_3", "a_6", split5Effect);
-        
-        
+
+        // Act
         var result = _gameBoard.TryExecuteSplitMove(firstMove, secondMove, 0);
+
+        // Assert
         Assert.True(result);
+        Assert.Equal("a_s1", _gameBoard.PawnTiles[0][0].Name);
+        Assert.Equal("a_6", _gameBoard.PawnTiles[0][1].Name);
     }
 
     [Fact]
@@ -66,8 +70,8 @@ public class GameBoardTest
         var split2Effect = GetMoveEffect("Split2");
         var split1Effect = GetMoveEffect("Split1");
         
-        _gameBoard.PawnTiles[0][0] = BoardTileDfs("a_1")!;
-        _gameBoard.PawnTiles[0][1] = BoardTileDfs("a_3")!;
+        _gameBoard.PawnTiles[0][0] = BoardTileDfs(_gameBoard, "a_1")!;
+        _gameBoard.PawnTiles[0][1] = BoardTileDfs(_gameBoard, "a_3")!;
         
         var firstMove = new GenericComponents.Move("a_1", "a_3", split2Effect);
         var secondMove = new GenericComponents.Move("a_3", "a_4", split1Effect);
@@ -79,13 +83,43 @@ public class GameBoardTest
     [Fact]
     public void TryExecuteMovePawn_ValidMove_ShouldReturnTrue()
     {
+        // Arrange
         var moveEffect = GetMoveEffect("Forward");
         var move = new GenericComponents.Move("a_4", "a_6", moveEffect);
-        
-        _gameBoard.PawnTiles[0][0] = BoardTileDfs("a_4")!;
 
+        _gameBoard.PawnTiles[0][0] = BoardTileDfs(_gameBoard, "a_4")!;
+
+        // Act
         var result = _gameBoard.TryExecuteMovePawn(move, CardDeck.CardTypes.Two, 0);
+
+        // Assert
         Assert.True(result);
+        Assert.Equal("a_6", _gameBoard.PawnTiles[0][0].Name);
+    }
+
+    [Fact]
+    public void TryExecuteMovePawn_WithPawnKill_ShouldSendEnemyToStart()
+    {
+        // Arrange
+        var moveEffect = GetMoveEffect("Forward");
+        var move = new GenericComponents.Move("a_4", "a_6", moveEffect);
+
+        // Setup player 0's pawn at a_4
+        _gameBoard.PawnTiles[0][0] = BoardTileDfs(_gameBoard, "a_4")!;
+
+        // Setup player 1's pawn at a_6 (destination)
+        _gameBoard.PawnTiles[1][0] = BoardTileDfs(_gameBoard, "a_6")!;
+
+        // Remember player 1's start tile
+        var player1StartTile = BoardTileDfs(_gameBoard, "b_S")!;
+
+        // Act
+        var result = _gameBoard.TryExecuteMovePawn(move, CardDeck.CardTypes.Two, 0);
+
+        // Assert
+        Assert.True(result);
+        Assert.Equal("a_6", _gameBoard.PawnTiles[0][0].Name);
+        Assert.Equal(player1StartTile.Name, _gameBoard.PawnTiles[1][0].Name);
     }
 
     [Fact]
@@ -93,35 +127,156 @@ public class GameBoardTest
     {
         var moveEffect = GetMoveEffect("ExitStart");
         var move = new GenericComponents.Move("a_4", "a_6", moveEffect);
-        _gameBoard.PawnTiles[0][0] = BoardTileDfs("a_6")!;
+        _gameBoard.PawnTiles[0][0] = BoardTileDfs(_gameBoard, "a_6")!;
 
         var result = _gameBoard.TryExecuteMovePawn(move, CardDeck.CardTypes.Two, 0);
         Assert.False(result);
     }
 
     [Fact]
+    public void FindTilesForApologiesMove_ShouldReturnValidTiles()
+    {
+        // Arrange
+        const string sourceTileName = "a_10";
+        const CardDeck.CardTypes card = CardDeck.CardTypes.Apologies;
+        const int playerIndex = 0;
+
+        var sourceTile = BoardTileDfs(_gameBoard, sourceTileName)!;
+        _gameBoard.PawnTiles[1][0] = BoardTileDfs(_gameBoard, "b_10")!;
+
+        // Act
+        var result = GetPrivateMethodResult<List<BoardTile>>(
+            "FindTilesForApologiesMove", sourceTile, card, playerIndex);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.True(result.Count >= 0);
+    }
+
+    [Fact]
+    public void FindTilesForSwapMove_ShouldReturnValidTiles()
+    {
+        // Arrange
+        const string sourceTileName = "a_10";
+        const CardDeck.CardTypes card = CardDeck.CardTypes.Eleven;
+        const int playerIndex = 0;
+
+        var sourceTile = BoardTileDfs(_gameBoard, sourceTileName)!;
+        _gameBoard.PawnTiles[1][0] = BoardTileDfs(_gameBoard, "b_12")!;
+
+        // Act
+        var result = GetPrivateMethodResult<List<BoardTile>>(
+            "FindTilesForSwapMove", sourceTile, card, playerIndex);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.True(result.Count >= 0);
+    }
+
+    [Fact]
+    public void FindTilesForSplitMove_ShouldReturnValidTiles()
+    {
+        // Arrange
+        const string sourceTileName = "a_10";
+        const CardDeck.CardTypes card = CardDeck.CardTypes.Seven;
+        const int playerIndex = 0;
+
+        var sourceTile = BoardTileDfs(_gameBoard, sourceTileName)!;
+        _gameBoard.PawnTiles[0][0] = sourceTile;
+        _gameBoard.PawnTiles[0][1] = BoardTileDfs(_gameBoard, "a_7")!;
+        
+        // Act
+        var result = GetPrivateMethodResult<List<(BoardTile, int)>>(
+            "FindTilesForSplitMove", sourceTile, card, playerIndex);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.True(result.Count > 0);
+    }
+
+    [Fact]
     public void ExecuteAnyAvailableSlides_ValidScenario_ShouldReassignPawns()
     {
+        // Arrange
         _gameBoard.ExecuteAnyAvailableSlides();
 
+        // Assert
         foreach (var tileSet in _gameBoard.PawnTiles) {
             Assert.All(tileSet, Assert.NotNull);
         }
     }
-    
-    
+
     [Fact]
-    public void ValidateAndFindDestinationTile_ValidMove_ShouldReturnDestinationTile()
+    public void ExecuteAnyAvailableSlides_WithPawnOnSlider_ShouldMovePawn()
+    {
+        // Arrange - Find a slider tile
+        var sliderTiles = new List<SliderTile>();
+        HashSet<string> visited = [];
+        Queue<BoardTile> queue = [];
+
+        foreach (var pawnSet in _gameBoard.PawnTiles)
+        foreach (var pawnTile in pawnSet) {
+            visited.Add(pawnTile.Name);
+            queue.Enqueue(pawnTile);
+        }
+
+        while (queue.Count > 0) {
+            var queueTile = queue.Dequeue();
+
+            if (queueTile is SliderTile slider) {
+                sliderTiles.Add(slider);
+            }
+
+            switch (queueTile) {
+                case WalkableTile walkableTile:
+                {
+                    for (var i = 0; i < 4; i++) {
+                        if (visited.Contains(walkableTile.EvaluateNextTile(i).Name)) continue;
+                        visited.Add(walkableTile.EvaluateNextTile(i).Name);
+                        queue.Enqueue(walkableTile.EvaluateNextTile(i));
+                    }
+                    break;
+                }
+                case StartTile startTile:
+                {
+                    visited.Add(startTile.NextTile.Name);
+                    queue.Enqueue(startTile.NextTile);
+                    break;
+                }
+            }
+        }
+
+        // Make sure we found at least one slider
+        Assert.True(sliderTiles.Count >= 2);
+
+        // Place a pawn on the slider
+        var sliderTile = sliderTiles[0];
+        var noMoveSliderTile = sliderTiles[1];
+        _gameBoard.PawnTiles[1][0] = sliderTile;
+        _gameBoard.PawnTiles[1][1] = noMoveSliderTile;
+        var targetTile = sliderTile.TargetTile;
+
+        // Act
+        _gameBoard.ExecuteAnyAvailableSlides();
+
+        // Assert
+        Assert.Equal(targetTile.Name, _gameBoard.PawnTiles[1][0].Name);
+        Assert.Equal(noMoveSliderTile.Name, _gameBoard.PawnTiles[1][1].Name);
+    }
+    
+    
+    [Theory]
+    [InlineData("a_S", "a_4", "ExitStart", CardDeck.CardTypes.One, 0)]
+    [InlineData("a_4", "a_6", "Forward", CardDeck.CardTypes.Two, 0)]
+    [InlineData("a_6", "a_2", "Backward", CardDeck.CardTypes.Four, 0)]
+    [InlineData("a_6", "a_5", "Backward", CardDeck.CardTypes.Ten, 0)]
+    public void ValidateAndFindDestinationTile_ValidMove_ShouldReturnDestinationTile(
+        string sourceTileName, string destTileName, string effectName, CardDeck.CardTypes cardType, int playerIndex)
     {
         // Arrange
-        const string sourceTileName = "a_S";
-        const string destTileName = "a_4";
-        const CardDeck.CardTypes cardType = CardDeck.CardTypes.One;
-        const int playerIndex = 0;
-        
-        var sourceTile = BoardTileDfs(sourceTileName)!;
-
-        var effect = GetMoveEffect("ExitStart");
+        var sourceTile = BoardTileDfs(_gameBoard, sourceTileName)!;
+        _gameBoard.PawnTiles[0][0] = sourceTile;
+        var effect = GetMoveEffect(effectName);
         var move = new GenericComponents.Move(sourceTileName, destTileName, effect);
 
         // Act
@@ -133,6 +288,54 @@ public class GameBoardTest
         Assert.Equal(destTileName, result.Name);
     }
 
+    [Theory]
+    [InlineData("a_4", "a_6", "Forward", CardDeck.CardTypes.One, 0, typeof(InvalidDataException))]
+    [InlineData("a_S", "a_10", "ExitStart", CardDeck.CardTypes.Three, 0, typeof(InvalidDataException))]
+    [InlineData("a_6", "a_5", "Backward", CardDeck.CardTypes.Two, 0, typeof(InvalidDataException))]
+    public void ValidateAndFindDestinationTile_InvalidMove_ShouldThrowException(
+        string sourceTileName, string destTileName, string effectName, CardDeck.CardTypes cardType, 
+        int playerIndex, Type expectedExceptionType)
+    {
+        // Arrange
+        var sourceTile = BoardTileDfs(_gameBoard, sourceTileName)!;
+        _gameBoard.PawnTiles[0][0] = sourceTile;
+        var effect = GetMoveEffect(effectName);
+        var move = new GenericComponents.Move(sourceTileName, destTileName, effect);
+
+        // Act & Assert
+        var ex = Assert.Throws<TargetInvocationException>(() => GetPrivateMethodResult<BoardTile>(
+            "ValidateAndFindDestinationTile", sourceTile, move, cardType, playerIndex));
+        Assert.IsType(expectedExceptionType, ex.InnerException);
+    }
+
+
+    [Theory]
+    [InlineData("a_4", CardDeck.CardTypes.One, 0, 1, "a_5")]
+    [InlineData("a_4", CardDeck.CardTypes.Two, 0, 1, "a_6")]
+    [InlineData("a_4", CardDeck.CardTypes.Three, 0, 1, "a_7")]
+    [InlineData("a_10", CardDeck.CardTypes.Four, 0, 0, null)]
+    [InlineData("a_10", CardDeck.CardTypes.Five, 0, 1, "a_15")]
+    [InlineData("a_10", CardDeck.CardTypes.Seven, 0, 1, "b_2")]
+    [InlineData("a_10", CardDeck.CardTypes.Eight, 0, 1, "b_3")]
+    [InlineData("a_10", CardDeck.CardTypes.Ten, 0, 1, "b_5")]
+    [InlineData("a_10", CardDeck.CardTypes.Eleven, 0, 1, "b_6")]
+    [InlineData("a_10", CardDeck.CardTypes.Twelve, 0, 1, "b_7")]
+    [InlineData("a_10", CardDeck.CardTypes.Apologies, 0, 0, null)]
+    public void FindTileForForwardMove_ValidScenario_ShouldReturnCorrectTiles(
+        string sourceTileName, CardDeck.CardTypes card, int playerIndex, int expectedTileCount, string? destTileName)
+    {
+        // Arrange
+        var sourceTile = BoardTileDfs(_gameBoard, sourceTileName)!;
+
+        // Act
+        var result = GetPrivateMethodResult<List<BoardTile>>(
+            "FindTileForForwardMove", sourceTile, card, playerIndex);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(expectedTileCount, result.Count);
+        Assert.Equal(destTileName, result.FirstOrDefault()?.Name);
+    }
 
     [Fact]
     public void ProcessDestinationTile_ValidScenario_ShouldHandleCorrectly()
@@ -141,6 +344,66 @@ public class GameBoardTest
         var destTile = new StartTile("a_2", null!);
 
         InvokePrivateMethod("ProcessDestinationTile", sourceTile, destTile, false);
+    }
+
+    [Theory]
+    [InlineData("a_10", CardDeck.CardTypes.Four, 0, 1, "a_6")]
+    [InlineData("a_5", CardDeck.CardTypes.One, 0, 0, null)]
+    [InlineData("a_5", CardDeck.CardTypes.Four, 0, 1, "a_1")]
+    [InlineData("a_5", CardDeck.CardTypes.Ten, 0, 1, "a_4")]
+    public void FindTileForBackwardMove_ValidScenario_ShouldReturnCorrectTiles(
+        string sourceTileName, CardDeck.CardTypes card, int playerIndex, int expectedTileCount, string? destTileName)
+    {
+        // Arrange
+        var sourceTile = BoardTileDfs(_gameBoard, sourceTileName)!;
+
+        // Act
+        var result = GetPrivateMethodResult<List<BoardTile>>(
+            "FindTileForBackwardMove", sourceTile, card, playerIndex);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(expectedTileCount, result.Count);
+        Assert.Equal(destTileName, result.FirstOrDefault()?.Name);
+    }
+
+    [Theory]
+    [InlineData("a_S", CardDeck.CardTypes.One, 0, 1)]
+    [InlineData("a_S", CardDeck.CardTypes.Two, 0, 1)]
+    [InlineData("a_S", CardDeck.CardTypes.Apologies, 0, 0)]
+    public void FindTileForExitStartMove_ValidScenario_ShouldReturnCorrectTiles(
+        string sourceTileName, CardDeck.CardTypes card, int playerIndex, int expectedTileCount)
+    {
+        // Arrange
+        var sourceTile = BoardTileDfs(_gameBoard, sourceTileName)!;
+
+        // Act
+        var result = GetPrivateMethodResult<List<BoardTile>>(
+            "FindTileForExitStartMove", sourceTile, card, playerIndex);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(expectedTileCount, result.Count);
+    }
+
+    [Theory]
+    [InlineData("a_4", 0, true)]
+    [InlineData("a_4", 1, true)]
+    [InlineData("c_3", 0, false)]
+    [InlineData("c_3", 1, true)]
+    public void NoTeammateOnTargetTile_ShouldReturnExpectedResult(
+        string tileName, int playerIndex, bool expected)
+    {
+        // Arrange
+        _gameBoard.PawnTiles[0][1] = BoardTileDfs(_gameBoard, "c_3")!;
+        var tile = BoardTileDfs(_gameBoard, tileName)!;
+
+        // Act
+        var result = GetPrivateMethodResult<bool>(
+            "NoTeammateOnTargetTile", tile, playerIndex);
+
+        // Assert
+        Assert.Equal(expected, result);
     }
 
     [Fact]
@@ -175,11 +438,11 @@ public class GameBoardTest
         return (int)Enum.Parse(moveEffect, value);
     }
 
-    private BoardTile? BoardTileDfs(string targetTileName)
+    internal static BoardTile? BoardTileDfs(GameBoard gameBoard, string targetTileName)
     {
         HashSet<string> visited = [];
         Queue<BoardTile> queue = [];
-        foreach (var pawnSet in _gameBoard.PawnTiles) {
+        foreach (var pawnSet in gameBoard.PawnTiles) {
             foreach (var pawnTile in pawnSet) {
                 visited.Add(pawnTile.Name);
                 queue.Enqueue(pawnTile);
